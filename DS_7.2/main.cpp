4b55f4a70b5a82410e6b05e55f36089fe6619698
陈翰 <chenhan199781@gmail.com>
2018-06-07 20:42:49 +0800

    Initial Commit

---
+++ DS_7.2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DS_7.2
+//
+//  Created by Walter Mitty on 2018/6/7.
+//  Copyright © 2018年 Walter Mitty. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
